acf84a1f7291e4f8796c054bf2c266905af43251
allensarmiento <allensarmiento@csu.fullerton.edu>
2019-02-08 00:08:11 -0800

    points decreasing and animation

--- boss-battle.py
+++ boss-battle.py
@@ -4,8 +4,6 @@ try:
 except ImportError:
     from tkinter import *
 
-pygame.init()
-
 # RGB Colors
 black = (0, 0, 0)
 silver = (211, 211, 211)
@@ -13,32 +11,44 @@ darkgrey = (169, 169, 169)
 grey = (128, 128, 128)
 white = (255, 255, 255)
 red = (255, 0, 0)
+darkred = (204, 0, 0)
 yellow = (255, 255, 0)
 green = (0, 255, 0)
+springgreen = (51, 255, 51)
 blue = (0, 0, 255)
+lightblue = (0, 255, 255)
+
+pygame.init()
 
 
-###### CUSTOMIZATIONS #######
-# variables to edit quickly #
-display_width = 1366
-display_height = 768
+# -------- Customizations ------------
+# Display settings
+display_width = 1600
+display_height = 900
 
+# Background color
 background_color = black
+
+# Button colors
 button_color = grey
 button_hover_color = yellow
 input_color = white
 input_hover = grey
 
+# Game Title
 game_title = "Star Wars: EpSIode 2019"
 
-title_screen_img_path = "starwarslogo.png"
+# Images
+title_screen_img_path = "star_wars.png"
 boss_image_path = "darth.png"
+game_bg_path = "starwarsspace.jpg"
 
+# Textfile
 boss_filename = "boss-info.txt"
 boss_info = open(boss_filename, 'r')
 print(boss_info.read())
 
-#Team names and images
+# Team names and images
 team1_name = "One"
 team1_image = ""
 
@@ -47,8 +57,16 @@ team2_image = ""
 
 team3_name = "Three"
 team3_image = ""
-#############################
+# --------------------------------
 
+def title_image(x, y):
+    gameDisplay.blit(title_img, (x, y))
+
+def game_background(x, y):
+    gameDisplay.blit(game_bg, (x, y))
+
+def boss_image(x, y):
+    gameDisplay.blit(boss_img, (x, y))
 
 def render_text(text, font, color):
     textSurface = font.render(text, True, color)
@@ -57,10 +75,8 @@ def render_text(text, font, color):
 def button(msg, x, y, width, height, hover_color, original_color, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-
     if x+width > mouse[0] > x and y+height > mouse[1] > y:
         pygame.draw.rect(gameDisplay, hover_color, (x,y,width,height))
-        # If left click on the button, then call the action
         if click[0] == 1 and action != None:
             if action == "play":
                 game_loop()
@@ -69,43 +85,40 @@ def button(msg, x, y, width, height, hover_color, original_color, action=None):
                 quit()
     else:
         pygame.draw.rect(gameDisplay, original_color, (x,y,width,height))
-
-    # Add the button
     buttonText = pygame.font.Font("freesansbold.ttf", 20)
     textSurf, textRect = render_text(msg, buttonText, black)
     textRect.center = ( (x+(width/2)), (y+(height/2)) )
     gameDisplay.blit(textSurf, textRect)
 
-
-def title_image(x, y):
-    gameDisplay.blit(title_img, (x, y))
-
 def game_intro():
     gameDisplay.fill(background_color)
     title_image(x, y)
-
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
-        button("Play", 350, 550, 150, 50, button_hover_color, button_color, "play")
-        button("Quit", 800, 550, 150, 50, button_hover_color, button_color, "quit")
-
+        button("Play", 500, 600, 150, 50, button_hover_color, button_color, "play")
+        button("Quit", 900, 600, 150, 50, button_hover_color, button_color, "quit")
         pygame.display.update()
         clock.tick(60)
 
-
-def boss_image(x, y):
-    gameDisplay.blit(boss_img, (x, y))
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([10, 10])
+        self.image.fill(springgreen)
+        self.rect = self.image.get_rect()
+    
+    def update(self):
+        self.rect.x += 2
 
 def game_loop():
-    gameDisplay.fill(background_color)
-
     game = True
+    gameDisplay.fill(background_color)
+    game_background(0, 0)
 
-    # Handling input boxes
+    # --- Handling input boxes
     font = pygame.font.Font(None, 32)
     team_1_color_inactive = pygame.Color('ghostwhite')
     team_1_color_active = pygame.Color('gold1')
@@ -123,18 +136,23 @@ def game_loop():
     team_3_text = ''
     team_3_active = False
 
-    # Boss health
-    boss_1_health = 250
-    boss_2_health = 250
-    boss_3_health = 250
+    # --- Boss health
+    boss_1_health = 300
+    boss_2_health = 300
+    boss_3_health = 300
+
+    # Bullets list
+    bullet_list = pygame.sprite.Group()
+
+    team_1_damage = 0
+    team_2_damage = 0
+    team_3_damage = 0
 
     while game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
-            # Check for button clicks in the input boxes
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if team_1.collidepoint(event.pos):
                     team_1_active = True
@@ -156,152 +174,169 @@ def game_loop():
                 else:
                     team_3_active = False
                     team_3_color = team_3_color_inactive
-
-            # Read keyboard presses. This only reads one keyboard press at
-            # a time.
             if event.type == pygame.KEYDOWN:
                 if team_1_active:
                     if event.key == pygame.K_RETURN:
                         print("Team 1: " + team_1_text)
-                        damage_amount = int(team_1_text)
-                        boss_1_health -= damage_amount
-                        if boss_1_health < 0:
-                            boss_1_health = 0
+                        team_1_damage = int(team_1_text)
                         team_1_text = ''
                     elif event.key == pygame.K_BACKSPACE:
                         team_1_text = team_1_text[:-1]
                     else:
                         team_1_text += event.unicode
-
                 if team_2_active:
                     if event.key == pygame.K_RETURN:
                         print("Team 2: " + team_2_text)
-                        damage_amount = int(team_2_text)
-                        boss_2_health -= damage_amount
-                        if boss_2_health < 0:
-                            boss_2_health = 0
+                        team_2_damage = int(team_2_text)
                         team_2_text = ''
                     elif event.key == pygame.K_BACKSPACE:
                         team_2_text = team_2_text[:-1]
                     else:
                         team_2_text += event.unicode
-
                 if team_3_active:
                     if event.key == pygame.K_RETURN:
                         print("Team 3: " + team_3_text)
-                        damage_amount = int(team_3_text)
-                        boss_3_health -= damage_amount
-                        if boss_3_health < 0:
-                            boss_3_health = 0
+                        team_3_damage = int(team_3_text)
                         team_3_text = ''
                     elif event.key == pygame.K_BACKSPACE:
                         team_3_text = team_3_text[:-1]
                     else:
                         team_3_text += event.unicode
+        game_background(0, 0)
 
-        gameDisplay.fill((30, 30, 30))
+        if team_1_damage > 0:
+            bullet = Bullet()
+            bullet.rect.x = 225
+            bullet.rect.y = 150
+            bullet_list.add(bullet)
+            team_1_damage -= 1
+        
+        if team_2_damage > 0:
+            bullet = Bullet()
+            bullet.rect.x = 225
+            bullet.rect.y = 400
+            bullet_list.add(bullet)
+            team_2_damage -= 1
+        
+        if team_3_damage > 0:
+            bullet = Bullet()
+            bullet.rect.x = 225
+            bullet.rect.y = 675
+            bullet_list.add(bullet)
+            team_3_damage -= 1
 
-        # Displaying team 1 items
-        team_1 = pygame.Rect(100, 225, 150, 25)
+        # Bullet spacing
+        for i in range(0, 7):
+            bullet_list.update()
+
+        for bullet in bullet_list:
+            if bullet.rect.x > display_width-450 and bullet.rect.y == 150:
+                bullet_list.remove(bullet)
+                boss_1_health -= 1
+                if boss_1_health < 0:
+                    boss_1_health = 0
+            if bullet.rect.x > display_width-450 and bullet.rect.y == 400:
+                bullet_list.remove(bullet)
+                boss_2_health -= 1
+                if boss_2_health < 0:
+                    boss_2_health = 0
+            if bullet.rect.x > display_width-450 and bullet.rect.y == 675:
+                bullet_list.remove(bullet)
+                boss_3_health -= 1
+                if boss_3_health < 0:
+                    boss_3_health = 0
+        
+        bullet_list.draw(gameDisplay)
+
+        # --- Team items
+        team_1 = pygame.Rect(150, 280, 150, 25)
         team_1_txtsurf = font.render(team_1_text, True, team_1_color)
         team_1_width = max(200, team_1_txtsurf.get_width() + 10)
         team_1.w = team_1_width
         gameDisplay.blit(team_1_txtsurf, (team_1.x+5, team_1.y+5))
         pygame.draw.rect(gameDisplay, team_1_color, team_1, 2)
-        # Team 1 team icon box
-        pygame.draw.rect(gameDisplay, team_1_color, (100, 25, 200, 175))
+        
+        pygame.draw.rect(gameDisplay, team_1_color, (150, 80, 200, 175))
 
-        # Displaying team 2 items
-        team_2 = pygame.Rect(100, 475, 150, 25)
+        team_2 = pygame.Rect(150, 550, 150, 25)
         team_2_txtsurf = font.render(team_2_text, True, team_2_color)
         team_2_width = max(200, team_2_txtsurf.get_width() + 10)
         team_2.w = team_2_width
         gameDisplay.blit(team_2_txtsurf, (team_2.x+5, team_2.y+5))
         pygame.draw.rect(gameDisplay, team_2_color, team_2, 2)
-        # Team 2 team icon box
-        pygame.draw.rect(gameDisplay, team_2_color, (100, 275, 200, 175))
+        
+        pygame.draw.rect(gameDisplay, team_2_color, (150, 350, 200, 175))
 
-        # Displaying team 3 items
-        team_3 = pygame.Rect(100, 725, 150, 25)
+        team_3 = pygame.Rect(150, 825, 150, 25)
         team_3_txtsurf = font.render(team_3_text, True, team_3_color)
         team_3_width = max(200, team_3_txtsurf.get_width() + 10)
         team_3.w = team_3_width
         gameDisplay.blit(team_3_txtsurf, (team_3.x+5, team_3.y+5))
         pygame.draw.rect(gameDisplay, team_3_color, team_3, 2)
-        # Team 3 team icon box
-        pygame.draw.rect(gameDisplay, team_3_color, (100, 525, 200, 175))
+        
+        pygame.draw.rect(gameDisplay, team_3_color, (150, 625, 200, 175))
 
+        # --- Boss
         bossText = pygame.font.Font("freesansbold.ttf", 20)
 
-        # Team 1 Boss
-        team_1_boss = pygame.Rect(1100, 200, boss_1_health, 25)
-        team_1_boss_max_health = pygame.Rect(1100, 200, 250, 25)
-        pygame.draw.rect(gameDisplay, grey, team_1_boss_max_health, 2)
-        pygame.draw.rect(gameDisplay, red, team_1_boss)
-        boss_image(1100, 45)
-        boss_1_textSurf, boss_1_textRect = render_text("Health: " + str(boss_1_health), bossText, grey)
-        boss_1_textRect.center = ( (1100-75, 215) )
+        team_1_boss = pygame.Rect(1100, 280, boss_1_health, 25)
+        team_1_boss_max_health = pygame.Rect(1100, 280, 300, 25)
+        pygame.draw.rect(gameDisplay, grey, team_1_boss_max_health)
+        pygame.draw.rect(gameDisplay, darkred, team_1_boss)
+        boss_image(1150, 80)
+        boss_1_textSurf, boss_1_textRect = render_text("Health: " + str(boss_1_health), bossText, white)
+        boss_1_textRect.center = ( (1100-75, 280+15) )
         gameDisplay.blit(boss_1_textSurf, boss_1_textRect)
 
-        # Team 2 Boss
-        team_2_boss = pygame.Rect(1100, 425, boss_2_health, 25)
-        team_2_boss_max_health = pygame.Rect(1100, 425, 250, 25)
-        pygame.draw.rect(gameDisplay, grey, team_2_boss_max_health, 2)
-        pygame.draw.rect(gameDisplay, red, team_2_boss)
-        boss_image(1100, 270) 
-        boss_2_textSurf, boss_2_textRect = render_text("Health: " + str(boss_2_health), bossText, grey)
-        boss_2_textRect.center = ( (1100-75, 440) )
+        team_2_boss = pygame.Rect(1100, 550, boss_2_health, 25)
+        team_2_boss_max_health = pygame.Rect(1100, 550, 300, 25)
+        pygame.draw.rect(gameDisplay, darkgrey, team_2_boss_max_health)
+        pygame.draw.rect(gameDisplay, darkred, team_2_boss)
+        boss_image(1150, 350) 
+        boss_2_textSurf, boss_2_textRect = render_text("Health: " + str(boss_2_health), bossText, white)
+        boss_2_textRect.center = ( (1100-75, 550+15) )
         gameDisplay.blit(boss_2_textSurf, boss_2_textRect)
 
-        # Team 3 Boss
-        team_3_boss = pygame.Rect(1100, 675, boss_3_health, 25)
-        team_3_boss_max_health = pygame.Rect(1100, 675, 250, 25)
-        pygame.draw.rect(gameDisplay, grey, team_3_boss_max_health, 2)
-        pygame.draw.rect(gameDisplay, red, team_3_boss)
-        boss_image(1100, 520)
-        boss_3_textSurf, boss_3_textRect = render_text("Health: " + str(boss_3_health), bossText, grey)
-        boss_3_textRect.center = ( (1100-75, 690) )
+        team_3_boss = pygame.Rect(1100, 820, boss_3_health, 25)
+        team_3_boss_max_health = pygame.Rect(1100, 820, 300, 25)
+        pygame.draw.rect(gameDisplay, grey, team_3_boss_max_health)
+        pygame.draw.rect(gameDisplay, darkred, team_3_boss)
+        boss_image(1150, 620)
+        boss_3_textSurf, boss_3_textRect = render_text("Health: " + str(boss_3_health), bossText, white)
+        boss_3_textRect.center = ( (1100-75, 820+15) )
         gameDisplay.blit(boss_3_textSurf, boss_3_textRect)
 
         pygame.display.update()
         clock.tick(60)
 
-
-# Set the window size and title
+# --------------- Main ---------------------------
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption(game_title)
 
-# Set the game clock
 clock = pygame.time.Clock()
 
-# Title screen image
-# NOTE: Change the value in order to center the image how you would like.
 title_img = pygame.image.load(title_screen_img_path)
-title_img = pygame.transform.scale(title_img, (display_width-500, display_height-250))
+title_img = pygame.transform.scale(title_img, (display_width-600, display_height-350))
 
-boss_img = pygame.image.load(boss_image_path)
-boss_img = pygame.transform.scale(boss_img, (150, 150))
+boss_img = pygame.image.load(boss_image_path).convert()
+transColor = boss_img.get_at((0, 0))
+boss_img.set_colorkey(transColor)
+boss_img = pygame.transform.scale(boss_img, (175, 175))
+
+game_bg = pygame.image.load(game_bg_path).convert()
+game_bg = pygame.transform.scale(game_bg, (display_width, display_height))
 
 # Location for the image to be placed
-# NOTE: Change these values to center the image how you would like.
 (x, y) = (display_width//6, display_height//10)
 
-# Loop for intro screen
 intro = True
-
-# Game Loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # Keep track of mouse movement
     mouse = pygame.mouse.get_pos()
-
-    # Start the game intro
     game_intro()
 
-# If we reach this point, quit the program
 pygame.quit()
 quit()
